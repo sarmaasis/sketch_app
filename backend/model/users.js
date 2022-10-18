1ac0c800d5238d515e4fa025dbf0e6075722e423
sarmaasis <sarmaasis@gmail.com>
2022-10-18 06:03:30 +0530

    git initiatized and DB schema for user registration

---
+++ backend/model/users.js
@@ -0,0 +1,30 @@
+//Import mongoose
+const mongoose = require('mongoose');
+
+// DB schema for user registration with fields { firstName, LastName, email, password }
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    }
+});
+
+
+module.exports = mongoose.model('User',UserSchema)
